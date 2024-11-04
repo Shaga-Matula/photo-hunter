2d2eef74091d3919f2a6a2178d3bdfd2e0f6a1ee
Paul-Gleeson <Shagamatula@gmail.com>
2024-11-04 18:24:57 +0000

    Test

--- PhotoHunter/settings.py
+++ PhotoHunter/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-shagamatula-photohunter-hav945uv49w.ws-eu107.gitpod.io','photo-hunter-c27362557b2b.herokuapp.com']
+ALLOWED_HOSTS = ['https://8000-shagamatula-photohunter-hav945uv49w.ws-eu116.gitpod.io','photo-hunter-c27362557b2b.herokuapp.com']
 
 
 # Application definition
@@ -131,6 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
